574b8d7de6ed86727a0fe011c5e1c22bb8d6d80c
Ahmed Metwaly <ametwaly@Ahmeds-MacBook-Pro.local>
2020-02-16 12:02:26 +0100

    msk updates

--- bin/msk.ts
+++ bin/msk.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { MskStack } from '../lib/msk-stack';
 
 const app = new cdk.App();
-new MskStack(app, 'MskStack');
+new MskStack(app, 'MskStack', {
+    env: {
+        account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
+    }
+});
--- lib/msk-stack.ts
+++ lib/msk-stack.ts
@@ -1,9 +1,58 @@
 import * as cdk from '@aws-cdk/core';
+import * as msk from '@aws-cdk/aws-msk'
+import * as ec2 from '@aws-cdk/aws-ec2'
+
 
 export class MskStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const clusterVpc = new ec2.Vpc(this, 'clusterVpc', {
+      cidr: '10.0.0.0/16',
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+        cidrMask: 24,
+        name: 'ingress',
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      {
+        cidrMask: 24,
+        name: 'internal',
+        subnetType: ec2.SubnetType.ISOLATED,
+      }
+      ]
+    });
+
+    //TODO : add ingress rule with specific IP
+
+    const subnetsIds = clusterVpc.publicSubnets.map(subnet=>subnet.subnetId);
+
+    const kafka = new msk.CfnCluster(this,'kafkacluster',{
+      clusterName:'test-cluster',
+      numberOfBrokerNodes:3,
+      kafkaVersion:'2.2.1',
+      encryptionInfo:{
+        encryptionInTransit:{
+          clientBroker : 'TLS_PLAINTEXT'
+        }
+      },
+      brokerNodeGroupInfo:{
+        instanceType:'kafka.m5.large',
+        clientSubnets:subnetsIds,
+        storageInfo:{
+          ebsStorageInfo:{
+            volumeSize:100
+          }
+        }
+      } 
+    })
+   
   }
 }
+
+//bin/kafka-topics.sh --create --zookeeper z-3.test-cluster.reldhe.c4.kafka.us-east-1.amazonaws.com:2181 --replication-factor 3 --partitions 1 --topic test
+
+//./kafka-topics.sh --describe --zookeeper  z-3.test-cluster.reldhe.c4.kafka.us-east-1.amazonaws.com:2181 --topic test
